c5306a693adca044d9c4bda14b3e502e2e1e4cfd
Tim Nicholls <tim.nicholls@stfc.ac.uk>
2019-02-21 16:46:45 +0000

    Add future package to requirements

--- setup.py
+++ setup.py
@@ -2,23 +2,24 @@
 
 import sys
 from setuptools import setup, find_packages
-#import versioneer
+import versioneer
 
-#with open('requirements.txt') as f:
-#   required = f.read().splitlines()
+with open('requirements.txt') as f:
+   required = f.read().splitlines()
 
 setup(name='fem',
-      #version=versioneer.get_version(),
-      #cmdclass=versioneer.get_cmdclass(),
+      version=versioneer.get_version(),
+      cmdclass=versioneer.get_cmdclass(),
       description='STFC FEM client framework',
-      url='https//github.com/odin-detector/odin-data',
+      url='https://github.com/stfc-aeg/fem-platform.git',
       author='Tim Nicholls',
       author_email='tim.nicholls@stfc.ac.uk',
       packages=find_packages(),
-      #install_requires=required,
+      install_requires=required,
       entry_points={
         'console_scripts': [
             'fem_shell = fem.utils.shell:main',
+            'fem_reconfig_ip = fem.utils.reconfig_ip:main'
          ]
       },
       zip_safe=False,
